ee69741241178cb4612721fcb9e5501e46a520d6
09979-ship-it <09979@khamphoempittaya.ac.th>
2026-09-21 16:49:07 +0700

    Add files via upload

---
+++ phanumatkkk.3.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'bio-epidemic-v3';
+const ASSETS = [
+  'PHANUMATkkk.1.html',
+  'PHANUMATkkk.2.json',
+  'PHANUMATkkk.3.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
